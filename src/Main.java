61d529eb27e42b6830dc2c0ffe2521e9a6d3db53
Pratibha2305 <pratibhaparihar135@gmail.com>
2024-06-06 09:31:16 +0530

    today update

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World my name is khan");
+       System.out.print("Hello Pratibha ");
+        System.out.print("Hello Pratibha");
+        System.out.println("Hello World my name is khan");
+        System.out.print("Hello Pratibha ");
+        System.out.print("Hello Pratibha");
+        System.out.print("hello");
+        System.out.print("hello\nworld\nwith\njava\nfrom\npratibha");
+        System.out.println("pratibha is best girl");
+        System.out.println("i love my india\nindia is best\npm modi become 3rd time pm");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*\n**");
+        System.out.println("**\n*");
+        System.out.println("***\n");
+        {
+
+
+        }
+    }
+}
